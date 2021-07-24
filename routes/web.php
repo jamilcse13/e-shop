f16fcd7de434a8f7fbc1a8a3c48aa5bb3a5368d4
jamil <jamil.cse13@gmail.com>
2021-07-25 00:40:03 +0600

    created product details page

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::view('/', 'site.pages.homepage');
 Route::get('/category/{slug}', 'Site\CategoryController@show')->name('category.show');
 
 Route::get('/product/{slug}', 'Site\ProductController@show')->name('product.show');
+Route::post('/product/add/cart', 'Site\ProductController@addToCart')->name('product.add.cart');
